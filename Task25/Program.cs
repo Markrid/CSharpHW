c9381978cb0062125e5de1e1ca0752178c2e3d63
Dmitry <marknmaxm@gmail.com>
2023-02-25 18:04:40 +0300

    fix1

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -4,6 +4,11 @@ Console.Write("Введите число A: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB<=0) 
+{
+    Console.Write($"Вы ввели неверное число");
+    return;
+}
 int result = 1;
 for (int i = 1; i <= numberB; i++)
 {
